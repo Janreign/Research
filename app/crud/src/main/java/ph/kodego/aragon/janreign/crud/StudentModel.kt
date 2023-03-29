a2570084c45fece254383a391140f76f0a7c3f64
Janreign <janreign.tibs@gmail.com>
2023-03-30 03:55:31 +0800

    Initial commit

---
+++ app/crud/src/main/java/ph/kodego/aragon/janreign/crud/StudentModel.kt
@@ -0,0 +1,16 @@
+package ph.kodego.aragon.janreign.crud
+
+import java.util.Random
+
+data class StudentModel(
+    var id: Int = getAutoId(),
+    var name: String = "",
+    var email:String = ""
+) {
+    companion object {
+        fun getAutoId():Int{
+            val random = Random()
+            return random.nextInt(100)
+        }
+    }
+}
